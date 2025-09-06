aa6cfdd506fc8f167c1e7f9d43868adffdbd1f1d
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-06 16:45:01 -0500

    implimented synthetic caching to demonstrate how great it can be for speed purposes and storing the data temporarily locally :)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var dataDownloader = new SlowDataDownloader();
+
+System.Console.WriteLine(dataDownloader.DownloadData("id1"));
+System.Console.WriteLine(dataDownloader.DownloadData("id2"));
+System.Console.WriteLine(dataDownloader.DownloadData("id3"));
+System.Console.WriteLine(dataDownloader.DownloadData("id1"));
+System.Console.WriteLine(dataDownloader.DownloadData("id3"));
+System.Console.WriteLine(dataDownloader.DownloadData("id1"));
+System.Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+Console.ReadKey();
+
+public class Cache<TKey, TData>
+{
+  private readonly Dictionary<TKey, TData> _cachedData = new();
+
+  public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime) {
+    if (!_cachedData.ContainsKey(key))
+    {
+      _cachedData[key] = getForTheFirstTime(key);
+    }
+      return _cachedData[key];
+  }
+}
+
+public interface IDataDownloader
+{
+  string DownloadData(string resouceId);
+}
+
+public class SlowDataDownloader : IDataDownloader
+{
+  private readonly Cache<string, string> _cache = new();
+
+  public string DownloadData(string resouceId)
+  {
+    return _cache.Get(resouceId, DownloadDataWithoutCaching);
+  }
+
+    private string DownloadDataWithoutCaching(string resouceId)
+  {
+    // let's imagine this method downloads real data,
+    // and it does it slowly
+    Thread.Sleep(1000);
+    return $"Some data for {resouceId}";
+  }
+}
